c1726e523f8fd9524278a4a583dd3b4efe90823d
iBra-extraSource <ibraheemaburumman483@gmail.com>
2026-09-13 19:04:21 +0300

    Server

--- src/server.js
+++ src/server.js
@@ -14,7 +14,8 @@ const predictionRoutes = require("./routes/predictionRoutes");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/clinics", clinicRoutes);
